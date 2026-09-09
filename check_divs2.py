399423f3e1700144af8d8e854b91ed9b1870331e
Komal Maurya <komalmauryagithub.com>
2026-09-09 10:32:02 +0530

    files deleted

--- check_divs2.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open('tme.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-start_idx = content.find('tmeTargetGstAmount')
-snippet = content[start_idx-200:start_idx+600]
-print(snippet)
